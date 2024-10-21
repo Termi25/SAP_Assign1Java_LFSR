d0fa5be5a20ebc080f95a9aa087cb563cf4b4e54
Termi25 <mariusrusu208@gmail.com>
2024-10-21 12:43:56 +0300

    Completed LFSR with integer

--- src/LFSR.java
+++ src/LFSR.java
@@ -1,17 +1,39 @@
 public class LFSR {
+    private static int register=0;
+
     public static void main(String[] args) {
-        //the register
-        int register = 0;
-        printRegister(register);
         byte[] initialSeed = {(byte) 0b10101010, (byte) 0b11110000, (byte) 0b00001111, (byte) 0b01010101};
         register = initRegister(initialSeed);
-        printRegister(register);
-        int leastBit = getLeastSignificantBit(register);
-        register = shiftAndInsertTapBit(register, applyTapSequence(register));
-        printRegister(register);
+
+        byte[] array=byteArrayGenerator(20);
+        System.out.println(getHexStringFromByteArray(array));
+
+        byte[] array2=byteArrayGenerator(50);
+        System.out.println(getHexStringFromByteArray(array2));
     }
 
-    private static int shiftAndInsertTapBit(int register, byte tapBit) {
+    private static byte[] byteArrayGenerator(int arraySize){
+        byte[] byteArray=new byte[arraySize];
+        for(int i=0;i<arraySize;i++){
+            int noBitsGenerated=0;
+            byteArray[i]=0x00;
+            while(noBitsGenerated<8){
+                byte generatedBit=fullStep();
+                byteArray[i]= (byte) (generatedBit<<noBitsGenerated | byteArray[i]);
+                noBitsGenerated++;
+            }
+            byteArray[i]=(byte)(byteArray[i] & 0xFF);
+        }
+        return byteArray;
+    }
+
+    private static byte fullStep(){
+        byte leastBit = getLeastSignificantBit(register);
+        register = shiftAndInsertTapBit(applyTapSequence(register));
+        return leastBit;
+    }
+
+    private static int shiftAndInsertTapBit(byte tapBit) {
         register = register >>> 1;
         register = register | (tapBit << 31);
         return register;
@@ -40,8 +62,6 @@ public class LFSR {
         int result=0;
         for(int i=3;i>=0;i--){
             result=result | ((initialValues[3-i] & 0xFF )<< (i*8));
-            //0xFF is needed because when you convert a negative value to a integer negative value;
-            //it counteracts what Java does when shifting negative numbers.
         }
         return result;
     }
@@ -49,5 +69,17 @@ public class LFSR {
     private static void printRegister(int register) {
         System.out.println("Register (binary): "+Integer.toBinaryString(register));
         System.out.println("Register (hex): "+Integer.toHexString(register).toUpperCase());
+    }
+
+    private static String getHexStringFromByte(byte value){
+        return String.format(" %02X",value);
+    }
+
+    private static String getHexStringFromByteArray(byte[] values){
+        StringBuilder sb=new StringBuilder();
+        for(byte value:values){
+            sb.append(String.format(" %02X",value));
+        }
+        return sb.toString();
     }
 }
